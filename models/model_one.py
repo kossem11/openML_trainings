212d221a24f2286ab4c5a9ed75ffa06239dcfdaa
kossem11 <kossem11@gmail.com>
2025-09-29 20:56:50 +0300

    rebuild1

--- models/model_one.py
+++ models/model_one.py
@@ -21,44 +21,52 @@ y , cat_inc = pd.factorize(df['income'])
 df.drop(['native.country','marital.status', 'education.num', 'income'], axis=1, inplace=True)
 
 #print(cat_races, cat_relations)
-print(df.info())
+#print(df.info())
 
 X_train, X_hold, y_train, y_hold = train_test_split(df.values, y, test_size=0.2, random_state=12)
 
-knn = KNeighborsClassifier(n_neighbors=10)
-knn.fit(X_train, y_train)
-knn_pred = knn.predict(X_hold)
-print(accuracy_score(y_hold, knn_pred))
+def SimpleKNN(X_train, X_hold, y_train, y_hold):
+    knn = KNeighborsClassifier(n_neighbors=10)
+    knn.fit(X_train, y_train)
+    knn_pred = knn.predict(X_hold)
+    return knn_pred, knn, accuracy_score(y_hold, knn_pred)
 
-tree = DecisionTreeClassifier (max_depth=5, random_state=12)
-tree.fit (X_train, y_train)
-tree_pred = tree.predict(X_hold)
-print(accuracy_score(y_hold, tree_pred))
+def SipleTree(X_train, X_hold, y_train, y_hold ):
+    tree = DecisionTreeClassifier (max_depth=5, random_state=12)
+    tree.fit (X_train, y_train)
+    tree_pred = tree.predict(X_hold)
+    return tree_pred, tree, accuracy_score(y_hold, tree_pred)
+
+def gridKNN(X_train, X_hold, y_train, y_hold):
+    knn_pipe = Pipeline([('scaler', StandardScaler()), ('knn', KNeighborsClassifier(n_jobs=-1))])
+    knn_params = {'knn__n_neighbors': range(1,10)}
+    knn_grid = GridSearchCV(knn_pipe, knn_params, cv=5, n_jobs=-1, verbose=True)
+    knn_grid.fit(X_train,y_train)
+    print(knn_grid.best_params_)
+    return knn_grid, accuracy_score(y_hold, knn_grid.predict(X_hold))
 
 #grid tree
-tree_params = {'max_depth': range(1,9), "max_features": range(4,10)}
-tree_grid = GridSearchCV(tree, tree_params, cv=5, n_jobs=-1, verbose=True)
-tree_grid.fit(X_train, y_train)
-print(tree_grid.best_params_)
-print(accuracy_score(y_hold, tree_grid.predict(X_hold)))
+def gridTree(X_train, X_hold, y_train, y_hold, tree):
 
-knn_pipe = Pipeline([('scaler', StandardScaler()), ('knn', KNeighborsClassifier(n_jobs=-1))])
-knn_params = {'knn__n_neighbors': range(1,10)}
-knn_grid = GridSearchCV(knn_pipe, knn_params, cv=5, n_jobs=-1, verbose=True)
-knn_grid.fit(X_train,y_train)
-print(knn_grid.best_params_)
-print(accuracy_score(y_hold, knn_grid.predict(X_hold)))
+    tree_params = {'max_depth': range(1,9), "max_features": range(4,10)}
+    tree_grid = GridSearchCV(tree, tree_params, cv=5, n_jobs=-1, verbose=True)
+    tree_grid.fit(X_train, y_train)
+    print(tree_grid.best_params_)
+    print(accuracy_score(y_hold, tree_grid.predict(X_hold)))
 
-dot_data = export_graphviz(
-    tree_grid.best_estimator_,
-    out_file=None,
-    feature_names=df.columns,
-    class_names=['Class 0', 'Class 1'],  # замените на ваши классы
-    filled=True,
-    rounded=True,
-    special_characters=True
-)
+    dot_data = export_graphviz(
+        tree_grid.best_estimator_,
+        out_file=None,
+        feature_names=df.columns,
+        class_names=['one','two'],
+        filled=True,
+        rounded=True,
+        special_characters=True
+    )
 
-graph = graphviz.Source(dot_data)
-graph.render("best_decision_tree")  # сохраняет как best_decision_tree.pdf
-graph.view()
+    graph = graphviz.Source(dot_data)
+    graph.render("best_decision_tree")
+    graph.view()
+
+_, X, _ = SipleTree(X_train, X_hold, y_train, y_hold)
+gridTree(X_train, X_hold, y_train, y_hold, X)
